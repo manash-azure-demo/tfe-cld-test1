c09de8e0d5281ad7da415b50e4239f76a1413c8f
manashdas2005 <68675012+manashdas2005@users.noreply.github.com>
2024-09-11 17:18:57 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,9 @@
 # Provider Configuration
 provider "azurerm" {
   features {}
-  subscription_id                 = var.subscription_id  # Reference from variables
+  subscription_id = var.subscription_id  # Reference from variables
+  client_id       = "625922cb-f5e3-4be6-8f56-f63f10cd3915"  # Use the appId from the output
+  client_secret   = "aWq8Q~Sw_CYFbDolJRju6QbqLKFpzydrC3Urb6R"  # Use the password from the output
+  tenant_id       = "82676786-5bc7-43c6-b8f8-b3ee02b0b5f3"  # Use the tenant from the output
   resource_provider_registrations = "none"  # Prevent automatic resource provider registration
 }
