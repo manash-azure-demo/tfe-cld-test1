9b351716d76d42cd4db8347e97db306ed6c0d449
manashdas2005 <68675012+manashdas2005@users.noreply.github.com>
2024-09-11 16:47:56 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features = {}
+
+  subscription_id = "4f6a6eb9-27d0-4ed6-a31c-2bde135e2db6"
+  client_id       = "e0f37fca-c3ec-4490-8a42-036916b0f93f"
+  client_secret   = "WNQ8Q~QzexCIBgLlPKpvyCfKeUOhxhWfG22NeaAS"
+  tenant_id       = "82676786-5bc7-43c6-b8f8-b3ee02b0b5f3"
+}
